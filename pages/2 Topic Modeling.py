1811a28e3388a21d635182bad8d2b6da0e1f5f06
Faizhal Arif S <126218330+faizhalas@users.noreply.github.com>
2023-03-08 11:14:08 +0700

    Update 2 Topic Modeling.py

--- pages/2 Topic Modeling.py
+++ pages/2 Topic Modeling.py
@@ -23,32 +23,30 @@ import streamlit.components.v1 as components
 from io import StringIO
 from nltk.stem.snowball import SnowballStemmer
 
+
+#===config===
 st.set_page_config(
      page_title="Coconut",
      page_icon="🥥",
      layout="wide"
 )
-
 st.header("pyLDA")
-   
-#subhead
 st.subheader('Put your CSV file and click generate')
 
+#===upload file===
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-    # Can be used wherever a "file-like" object is accepted:
     papers = pd.read_csv(uploaded_file)
-    #st.dataframe(scopus.style.where(lambda val: '[No abstract available]' in str(val), 'color: red', subset=['Abstract']))
     paper = papers.dropna(subset=['Abstract'])
     paper = paper[~paper.Abstract.str.contains("No abstract available")]
     paper = paper[~paper.Abstract.str.contains("STRAIT")]
         
-    #mapping
+    #===mapping===
     paper['Abstract_pre'] = paper['Abstract'].map(lambda x: re.sub('[,:;\.!?•-]', '', x))
     paper['Abstract_pre'] = paper['Abstract_pre'].map(lambda x: x.lower())
     paper['Abstract_pre'] = paper['Abstract_pre'].map(lambda x: re.sub('©.*', '', x))
         
-    #lemmatize
+    #===lemmatize===
     lemmatizer = WordNetLemmatizer()
     def lemmatize_words(text):
         words = text.split()
@@ -56,18 +54,18 @@ if uploaded_file is not None:
         return ' '.join(words)
     paper['Abstract_lem'] = paper['Abstract_pre'].apply(lemmatize_words)
        
-    #stopword removal
+    #===stopword removal===
     stop = stopwords.words('english')
     paper['Abstract_stop'] = paper['Abstract_lem'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop)]))
         
-    #topic
+    #===topic===
     topic_abs = paper.Abstract_stop.values.tolist()
     topic_abs = [t.split(' ') for t in topic_abs]
     id2word = Dictionary(topic_abs)
     corpus = [id2word.doc2bow(text) for text in topic_abs]
-
     num_topic = st.slider('Choose number of topics', min_value=2, max_value=15, step=1)
-    #LDA
+     
+    #===LDA===
     lda_model = LdaModel(corpus=corpus,
                 id2word=id2word,
                 num_topics=num_topic, #num of topic
@@ -79,13 +77,13 @@ if uploaded_file is not None:
     pprint(lda_model.print_topics())
     doc_lda = lda_model[corpus]
 
-#coherence score
+#===coherence score===
     if st.button('📐 Calculate coherence'):
                 with st.spinner('Calculating, please wait ....'):    
                    coherence_model_lda = CoherenceModel(model=lda_model, texts=topic_abs, dictionary=id2word, coherence='c_v')
                    coherence_lda = coherence_model_lda.get_coherence()
                    st.write(coherence_lda)
-
+#===visualization===
     if st.button('📈 Generate visualization'):
                 with st.spinner('Creating pyLDAvis Visualization ...'):
                     vis = pyLDAvis.gensim_models.prepare(lda_model, corpus, id2word)
