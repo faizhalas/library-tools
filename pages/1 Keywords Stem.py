794b2d07a1339fa64044f989a680fbbef13c206b
Faizhal Arif S <126218330+faizhalas@users.noreply.github.com>
2023-03-16 12:06:03 +0700

    Update 1 Keywords Stem.py

--- pages/1 Keywords Stem.py
+++ pages/1 Keywords Stem.py
@@ -7,17 +7,9 @@ nltk.download('wordnet')
 from nltk.stem import WordNetLemmatizer
 nltk.download('stopwords')
 from nltk.corpus import stopwords
-import gensim
-import gensim.corpora as corpora
-from gensim.corpora import Dictionary
-from gensim.models.coherencemodel import CoherenceModel
-from gensim.models.ldamodel import LdaModel
 from pprint import pprint
 import spacy
 import pickle
-import pyLDAvis
-import pyLDAvis.gensim_models as gensimvis
-import matplotlib.pyplot as plt
 import streamlit.components.v1 as components
 from io import StringIO
 from nltk.stem.snowball import SnowballStemmer
@@ -45,21 +37,27 @@ with col2:
 
 #===body===
 if uploaded_file is not None:
-     papers = pd.read_csv(uploaded_file)
-     keywords = papers.dropna(subset=[keyword])
-     datakey = keywords[keyword].map(lambda x: re.sub(' ', '_', x))
-     datakey = datakey.map(lambda x: re.sub('-—–', '_', x))
-     datakey = datakey.map(lambda x: re.sub(';_', ' ', x))
-     datakey = datakey.map(lambda x: x.lower())
+     keywords = pd.read_csv(uploaded_file)
+     keywords[keyx] = keywords[keyx].astype(str)
+     keywords[keyx] = keywords[keyx].map(lambda x: re.sub('-—–', ' ', x))
+     keywords[keyx] = keywords[keyx].map(lambda x: x.lower())
+     
+     #===Keywords list===
+     key = keywords[keyx]
+     key = key.dropna()
+     key = pd.concat([key.str.split('; ', expand=True)], axis=1)
+     key = pd.Series(np.ravel(key)).dropna().drop_duplicates().sort_values().reset_index()
+     key['new']=key[0]
      
      #===stem/lem===
      if method is 'Lemmatization':          
         lemmatizer = WordNetLemmatizer()
         def lemmatize_words(text):
              words = text.split()
-             words = [lemmatizer.lemmatize(word,pos='v') for word in words]
+             words = [lemmatizer.lemmatize(word) for word in words]
              return ' '.join(words)
-        datakey = datakey.apply(lemmatize_words)
+        keywords[keyx] = keywords[keyx].apply(lemmatize_words)
+        key['new'] = key['new'].apply(lemmatize_words)
              
      else:
         stemmer = SnowballStemmer("english")
@@ -67,23 +65,38 @@ if uploaded_file is not None:
             words = text.split()
             words = [stemmer.stem(word) for word in words]
             return ' '.join(words)
-        datakey = datakey.apply(stem_words)
-             #st.write(datakey)
-     datakey = datakey.map(lambda x: re.sub(' ', '; ', x))
-     datakey = datakey.map(lambda x: re.sub('_', ' ', x))
-     keywords[keyword] = datakey
+        keywords[keyx] = keywords[keyx].apply(stem_words)
+        key['new'] = key['new'].apply(stem_words)
      
-     st.write(keywords)
-     st.write('Congratulations! 🤩 You choose',keyword ,'with',method,'method. Now, you can easily download the result by clicking the button below')
+     st.write('Congratulations! 🤩 You choose',keyx ,'with',method,'method. Now, you can easily download the result by clicking the button below')
      
-     #===download csv===
-     def convert_df(df):
-        return df.to_csv(index=False).encode('utf-8')
+     #===show & download csv===
+     col1, col2 = st.columns(2)
+     with col1:
+         st.write(keywords, use_container_width=True)
+         def convert_df(df):
+            return df.to_csv(index=False).encode('utf-8')
 
-     csv = convert_df(keywords)
-     st.download_button(
-         "Press to Download 👈",
-         csv,
-         "scopus.csv",
-         "text/csv",
-         key='download-csv')
+         csv = convert_df(keywords)
+         st.download_button(
+             "Press to download result 👈",
+             csv,
+             "scopus.csv",
+             "text/csv")
+          
+     with col2:
+         keywords[keyx] = keywords[keyx].str.replace('nan','')
+         key = key.drop(['index'], axis=1).rename(columns={0: "old"})
+         st.write(key, use_container_width=True)
+                  
+         def convert_dfs(df):
+                return df.to_csv(index=False).encode('utf-8')
+
+         csv = convert_dfs(key)
+         st.download_button(
+             "Press to download keywords 👈",
+             csv,
+             "keywords.csv",
+             "text/csv")
+     
+     
