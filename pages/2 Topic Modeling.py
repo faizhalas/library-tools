3e149a8699898b4a04ef27b869133b27a5ca420a
Faizhal A. S <126218330+faizhalas@users.noreply.github.com>
2023-09-06 10:42:02 +0700

    Update 2 Topic Modeling.py

--- pages/2 Topic Modeling.py
+++ pages/2 Topic Modeling.py
@@ -44,6 +44,14 @@ st.set_page_config(
      layout="wide"
 )
 st.header("Topic Modeling")
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
 st.subheader('Put your file here...')
 
 #========unique id========
